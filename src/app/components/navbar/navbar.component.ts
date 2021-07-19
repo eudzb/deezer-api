81aa5914a9e01a3c0822fb43e121cefb07b6a78e
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2021-07-19 13:35:01 +0200

    Added search bar in navigation menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, HostListener} from '@angular/core';
 import {Router, NavigationEnd} from '@angular/router';
 import {filter} from 'rxjs/operators';
+import {DeezerRequestService} from '../../services/deezer-request.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,7 @@ import {filter} from 'rxjs/operators';
 
 export class NavbarComponent implements OnInit {
   actualUrl: string;
+  searchQuery: string = null;
 
   constructor(private router: Router) {
   }
@@ -27,16 +29,31 @@ export class NavbarComponent implements OnInit {
     const nav = document.querySelector('#nav-menu') as HTMLElement;
     const deezerLabel = document.querySelector('#deezer-label') as HTMLElement;
     const genresLabel = document.querySelector('.genres-label') as HTMLElement;
+    const searchIcon = document.querySelector('#search-icon') as HTMLElement;
+    const searchInputText = document.querySelector('#search-input-text') as HTMLElement;
     if (window.scrollY > 70) {
       nav.classList.add('bg-white');
       nav.classList.add('shadow-sm');
       deezerLabel.classList.add('text-dark');
       genresLabel.classList.add('text-dark');
+      searchIcon.classList.add('search-icon-dark');
+      searchInputText.classList.add('text-dark');
+      searchInputText.classList.add('search-input-dark');
     } else {
       nav.classList.remove('bg-white');
       nav.classList.remove('shadow-sm');
       deezerLabel.classList.remove('text-dark');
       genresLabel.classList.remove('text-dark');
+      searchIcon.classList.remove('search-icon-dark');
+      searchInputText.classList.remove('text-dark');
+      searchInputText.classList.remove('search-input-dark');
     }
+  }
+
+  onSearch() {
+    if (!this.searchQuery || this.searchQuery.trim() === '') {
+      return;
+    }
+    this.router.navigate(['/search/', this.searchQuery]);
   }
 }
